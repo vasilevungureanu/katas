4a6cf42766caadc775b6143ddb835baf40f80527
Vasile Ungureanu <vungureanu@4suites.nl>
2019-09-02 15:47:44 +0300

    Add additional test flavors

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,13 +20,15 @@ cp quality.sh .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
 
 echo 'Installing commit-msg git hook'
-curl https://cdn.rawgit.com/tommarshall/git-good-commit/v0.6.1/hook.sh >.git/hooks/commit-msg && chmod +x .git/hooks/commit-msg
+curl https://cdn.rawgit.com/tommarshall/git-good-commit/v0.6.1/hook.sh > .git/hooks/commit-msg && chmod +x .git/hooks/commit-msg
 
 echo 'Installing shellcheck'
-if [[ "${OSTYPE}" == "linux-gnu" ]]; then
-  sudo apt-get install shellcheck
-elif [[ "${OSTYPE}" == "darwin"* ]]; then
-  brew install shellcheck
+if [[ "${OSTYPE}" == "linux-gnu" ]];
+  then
+    sudo apt-get install shellcheck
+elif [[ "${OSTYPE}" == "darwin"* ]];
+  then
+    brew install shellcheck
 fi
 
 echo 'Bootstrap process completed'
