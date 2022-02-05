09007827b4b6f7c30186c0b73ebde728e57fcfb2
Vasile <vasile.v.ungureanu@gmail.com>
2022-02-05 11:43:23 +0200

    Format leap year

--- src/test/java/com/vasileungureanu/katas/leapyear/YearShould.java
+++ src/test/java/com/vasileungureanu/katas/leapyear/YearShould.java
@@ -9,29 +9,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class YearShould {
     @Test
     @DisplayName("not be a leap year if is not divisible by 4")
-    public void
-    notBeLeapYearIfNotDivisibleBy4() {
+    public void notBeLeapYearIfNotDivisibleBy4() {
         assertThat(leapYear(1997)).isFalse();
     }
 
     @Test
     @DisplayName("be a leap year if is divisible by 4")
-    public void
-    beLeapYearIfDivisibleBy4() {
+    public void beLeapYearIfDivisibleBy4() {
         assertThat(leapYear(1996)).isTrue();
     }
 
     @Test
     @DisplayName("be a leap year if is divisible by 400")
-    public void
-    beLeapYearIfDivisibleBy400() {
+    public void beLeapYearIfDivisibleBy400() {
         assertThat(leapYear(1600)).isTrue();
     }
 
     @Test
     @DisplayName("not be a leap year if is divisible by 100 but not by 400")
-    public void
-    notBeLeapYearIfDivisibleBy100ButNotBy400() {
+    public void notBeLeapYearIfDivisibleBy100ButNotBy400() {
         assertThat(leapYear(1800)).isFalse();
     }
 
